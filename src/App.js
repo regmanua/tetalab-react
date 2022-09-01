5e5f73b2f019554a10a501c5888e7587d8dc4d89
regmanua <awj7681@gmail.com>
2022-09-01 19:38:41 +0300

    final changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import './App.css';
 import Form from './Form';
 import Feedback from './Feedback';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+const API_URL = 'https://vn0ibo4ov0.execute-api.eu-central-1.amazonaws.com';
 
 function App() {
-
-  const API_URL = 'https://vn0ibo4ov0.execute-api.eu-central-1.amazonaws.com';
   const [feedbackList, setFeedBackList] = useState([]);
-  const postsList = feedbackList.map(post => {
-    return (
-      <div>
-        <Feedback key={post.id} post={post} handleDelete={() => deletePost(post.id)}/>
-      </div>
-    )
-  })
+  const [filter, setFilter] = useState('');
+  const [filteredList, setFilteredList] = useState([]);
 
+  console.log(`filter = `, filter);
 
   async function getAllPosts () {
     await fetch(`${API_URL}/posts`, {mode: 'cors'})
       .then((response) => response.json())
       .then(data => {
         setFeedBackList(data.Items);
+        setFilteredList(data.Items);
         console.log(`feedbackList`, feedbackList);
+        console.log(`fetchedList`, filteredList);
       })
       .catch((err) => {
          console.log(err.message);
       });
-  };  
+  };
 
   async function addPosts (formData) {
     await fetch(`${API_URL}/posts`, {
@@ -63,14 +61,36 @@ function App() {
       });
   };
 
+  const onChangeHandler = event => {
+    setFilter(event.target.value);
+    const newList = filteredList.filter(post => post.feedback.includes(filter));
+    setFeedBackList(newList);
+  };
+
   useEffect(() => {
-    getAllPosts();
+    getAllPosts()
+      .then(console.log("fetchedList", filteredList));
   }, []);
+
+  const postsList = feedbackList.map(post => {
+    return (
+      <div>
+        <Feedback key={post.id} post={post} handleDelete={() => deletePost(post.id)}/>
+      </div>
+    )
+  });
 
   return (
     <div className='container p-4'>
-      <header>
-        <div className='text-2xl font-medium py-4'>Feedback React Application</div>
+      <header className='flex justify-between items-center'>
+        <div className='flex flex-col'>
+          <div className='text-2xl font-medium pt-5'>Feedback React Application</div>
+          <span>Posts found: {feedbackList.length}</span>
+        </div>
+        <div>
+          <span className='pr-3'>Filter feedbacks by keyword:</span>
+          <input type="text" id="filter" onChange={onChangeHandler} value={filter}/>
+        </div>
       </header>
       {postsList}
       <Form handleAddPost={(formData) => addPosts(formData)}/>
